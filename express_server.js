28361508c2be45968f98e1d42413ef73dc44c075
Abigail Addae <abigail2addae@gmail.com>
2022-06-13 22:43:56 +0000

    Updated routes in express_server removing unused routes

--- express_server.js
+++ express_server.js
@@ -10,15 +10,11 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
