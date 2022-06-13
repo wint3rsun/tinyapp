b6e3de4d2af8043b0ac67a89fbdf699bd7715845
Abigail Addae <abigail2addae@gmail.com>
2022-06-13 23:05:49 +0000

    added get route for /urls/new and required body-parser in order to handle post requests

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -22,11 +25,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
